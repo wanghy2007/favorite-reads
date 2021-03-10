c8b556da2bc98d5bbfa445a6bb87203e0b7e93aa
Hong-Yi Wang <wanghy2007@gmail.com>
2021-03-10 10:12:32 -0800

    first commit

---
+++ src/main/java/com/moonbase/favoritereads/favoritereads/FavoriteReadsApplication.java
@@ -0,0 +1,13 @@
+package com.moonbase.favoritereads.favoritereads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteReadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteReadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moonbase/favoritereads/favoritereads/FavoriteReadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonbase.favoritereads.favoritereads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteReadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
