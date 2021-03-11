dd1b2b4c0d7eb29ddfb10cf46979f9a2ddbfa661
Hong-Yi Wang <wanghy2007@gmail.com>
2021-03-11 09:31:15 -0800

    Change package name

--- src/main/java/com/moonbase/nt/favoritereads/FavoriteReadsApplication.java
+++ src/main/java/com/moonbase/nt/favoritereads/FavoriteReadsApplication.java
@@ -1,4 +1,4 @@
-package com.moonbase.favoritereads.favoritereads;
+package com.moonbase.nt.favoritereads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/moonbase/nt/favoritereads/FavoriteReadsApplicationTests.java
+++ src/test/java/com/moonbase/nt/favoritereads/FavoriteReadsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.moonbase.favoritereads.favoritereads;
+package com.moonbase.nt.favoritereads;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
